be3087ccccdd4ade35ff3635d2c530aa8f044c96
Luis Boscan <boscanp.luis@gmail.com>
2017-04-04 21:13:25 -0400

    Create synergy component

---
+++ Assets/Scripts/Mechanics/Synergy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Synergy : MonoBehaviour {
+
+    public float maxAmount;
+    public float currentAmount;
+    public float recoverRate;
+    public float depleteRate;
+    public bool recovering;
+    public bool depleting;
+
+    void Update()
+    {
+        if (recovering)
+        {
+            Recover(recoverRate * Time.deltaTime);
+        }
+        if (depleting)
+        {
+            Consume(depleteRate * Time.deltaTime);
+        }
+    }
+
+    public bool Consume(float amount)
+    {
+        if (!enabled)
+        {
+            return false;
+        }
+        float absAmount = Mathf.Abs(amount);
+        if (currentAmount < absAmount)
+        {
+            return false;
+        }
+        currentAmount -= absAmount;
+        return false;
+    }
+
+    public void Recover(float amount)
+    {
+        if (!enabled)
+        {
+            return;
+        }
+        float absAmount = Mathf.Abs(amount);
+        currentAmount = Mathf.Min(maxAmount, currentAmount + absAmount);
+    }
+}
