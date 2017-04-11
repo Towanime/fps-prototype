67b025c2c2794cf78fffb45b04eaedc1b549c2cd
Dom <dominiklussier@gmail.com>
2017-04-10 20:56:13 -0400

    CAD base movement

--- Assets/Scripts/Dom Scripts/CadController.cs
+++ Assets/Scripts/Dom Scripts/CadController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CadController : MonoBehaviour {
 
+	public int positionState = 0;
+	public float speed;
+
+	public Transform targetRotation;
+	public Transform targetOne;
+	public Transform targetTwo;
+	public Transform targetThree;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +18,21 @@ public class CadController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		transform.LookAt(targetRotation);
+
+		float step = speed * Time.deltaTime;
+
+		if (positionState == 1) {
+			transform.position = Vector3.MoveTowards (transform.position, targetOne.position, step);
+		}
+		if (positionState == 2) {
+			transform.position = Vector3.MoveTowards (transform.position, targetTwo.position, step);
+		}
+		if (positionState == 3) {
+			transform.position = Vector3.MoveTowards (transform.position, targetThree.position, step);
+		}
+		if (positionState == 4) {
+			transform.position = Vector3.MoveTowards (transform.position, targetRotation.position, step);
+		}
 	}
 }
