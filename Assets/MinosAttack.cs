69a0ed2791bb9b6f2da912597362891d24c4c75d
Luis Boscan <boscanp.luis@gmail.com>
2017-04-10 20:56:18 -0400

    Add damage to minos attack

---
+++ Assets/MinosAttack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinosAttack : MonoBehaviour {
+
+    public float punchDamage = 25;
+
+    void DamagePlayer()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            player.GetComponent<DamageableEntity>().OnDamage(gameObject, punchDamage);
+        }
+    }
+}
